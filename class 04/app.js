73b5e1f8fd9bfffe0493d3487b230b5633423609
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2023-10-28 10:55:31 +0500

    Add files via upload

---
+++ class 04/app.js
@@ -0,0 +1,28 @@
+// let obj = {
+//     name:"faraz",
+//     age:22,
+//     address:{
+//         city:"karachi"
+//     }
+// }
+
+// console.log(Object.keys(obj))
+
+// console.log(Object.values(obj))
+
+// console.log(Object.entries(obj))
+
+
+let arr = ["faraz",12,true,{city:"karachi",age:20},false,100,"abc"];
+
+// console.log(arr[3].age)
+
+arr[7] = "jawan pakistan"
+console.log(arr)
+
+// let obj = {
+//     name:"faraz",
+//     age:22,
+// }
+
+// console.log(obj)
