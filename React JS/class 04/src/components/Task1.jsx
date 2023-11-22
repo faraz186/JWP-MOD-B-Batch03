4d469814ab7b1c911e39830b529a84f753438276
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2023-11-22 14:35:11 +0500

    Update Task1.jsx

--- React JS/class 04/src/components/Task1.jsx
+++ React JS/class 04/src/components/Task1.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 const Task1 = () => {
     let time = new Date().toLocaleDateString();
-  let date = new Date().toLocaleTimeString();
+    let date = new Date().toLocaleTimeString();
 
   let userName = "faraz"
   return (
     <div>
         <h1>Hello My name is {userName}</h1>
 
-<p>Current Date: {date}</p>
+        <p>Current Date: {date}</p>
 
-<p>Current Date: {time}</p>
+        <p>Current Date: {time}</p>
     </div>
   )
 }
