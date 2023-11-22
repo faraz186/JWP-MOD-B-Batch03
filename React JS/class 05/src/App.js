431650a5c4712ee183dd1e5caeced42fb6635e6f
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2023-11-22 14:38:35 +0500

    practice of props and hooks (useState)

--- React JS/class 05/src/App.js
+++ React JS/class 05/src/App.js
@@ -28,7 +28,6 @@ function App() {
 
       <button onClick={dec}>Decrement</button>
 
-
       <Cards description="dell laptop ha bhot acha" title="Dell laptop core i5 8th generation" imgSrc="https://static-01.daraz.pk/p/23788f6bf5f5ad7baca0d61b2a03f6e2.jpg_750x750.jpg_.webp" />
 
       <Cards description="hp laptop ha bhot acha" title="Hp laptop core i7 10th generation" imgSrc="https://m.media-amazon.com/images/I/419e+yV+jgL._AC_SY164_.jpg"/>
@@ -37,13 +36,11 @@ function App() {
 
       <Cards description="dell laptop ha bhot acha" title="dell laptop core 2 duo 10th generation" imgSrc="https://static.komputronik.pl/product-picture/6/LTI15ACNXK82EP001_10M232-1.jpg"/>
 
-
       <Button type="SignUp"/>
       <Button type="Login"/>
       <Button type="Profile"/>
       <Button type="Next"/>
       <Button type="Back"/>
-
 
     </div>
   );
