6e950092f8e3053194373bb13bd6eff3584c723f
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-01-08 16:06:18 +0500

    Update app.js

--- Advance JS/class 05/app.js
+++ Advance JS/class 05/app.js
@@ -3,7 +3,7 @@ var arr = [1,2,3,4,5,[24,true],6,["hamza","faraz"],7,8,9];
 for(var i = 0; i< arr.length;i++)
 {
     for(var j =0;j<arr[i].length;j++)
-    {
+    {    
         console.log(arr[i][j])
 
     }
