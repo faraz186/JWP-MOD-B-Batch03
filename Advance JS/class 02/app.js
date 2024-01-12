ebc6f2cd5fa476745ac9e460f5c7a543ccf2811c
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-01-12 14:40:19 +0500

    Update app.js

--- Advance JS/class 02/app.js
+++ Advance JS/class 02/app.js
@@ -63,26 +63,22 @@ const nestedObject = {
     key4: "value4",
   };
 
-// let {key1,key2,key3,key4} = nestedObject;
+let {key1,key2,key3,key4} = nestedObject;
 
-// let {subKey1,subKey2,subKey3,subKey4} = nestedObject.key3;
+let {subKey1,subKey2,subKey3,subKey4} = nestedObject.key3;
 
-// let {subSubKey1,subSubKey2,subSubKey3,subSubKey6} = nestedObject.key3.subKey3;
+let {subSubKey1,subSubKey2,subSubKey3,subSubKey6} = nestedObject.key3.subKey3;
 
 let {subSubKey4,subSubKey5} = nestedObject.key3.subKey3.subSubKey3;
 
-
-
 console.log(subSubKey4)
 console.log(subSubKey5)
-// console.log(subSubKey3)
-// console.log(subSubKey6)
+console.log(subSubKey3)
+console.log(subSubKey6)
 
-
-// console.log(key1)
-// console.log(key2)
-// console.log(key4)
-// console.log(subKey1)
-// console.log(subKey2)
-// console.log(subKey3)
-
+console.log(key1)
+console.log(key2)
+console.log(key4)
+console.log(subKey1)
+console.log(subKey2)
+console.log(subKey3)
