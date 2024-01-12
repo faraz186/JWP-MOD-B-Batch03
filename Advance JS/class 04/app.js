2114642d58d706b17f1c8d49a57f8a0dbd801efd
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-01-12 14:36:12 +0500

    Update app.js

--- Advance JS/class 04/app.js
+++ Advance JS/class 04/app.js
@@ -6,11 +6,11 @@ let obj = {
     }
 }
       
-// console.log(Object.keys(obj))
+console.log(Object.keys(obj))
 
-// console.log(Object.values(obj))
+console.log(Object.values(obj))
 
-// console.log(Object.entries(obj))
+console.log(Object.entries(obj))
 
 
 let arr = ["faraz",12,true,{city:"karachi",age:20},false,100,"abc"];
