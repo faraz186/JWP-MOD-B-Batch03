87e4792a4495a167ee7f1fc8a17ff6996683cd7c
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-01-15 00:05:05 +0500

    Update app.js

--- Advance JS/class 05/app.js
+++ Advance JS/class 05/app.js
@@ -4,7 +4,7 @@ for(var i = 0; i< arr.length;i++)
 {    
     for(var j =0;j<arr[i].length;j++)
     {        
-        console.log(arr[i][j])
+        console.log(arr[i][j])   
     
     }   
 }   
