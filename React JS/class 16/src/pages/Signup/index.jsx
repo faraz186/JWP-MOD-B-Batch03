bfd7cbec7bbb2ec7618d994bf52c354081914fa5
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-01-17 11:48:56 +0500

    Update index.jsx

--- React JS/class 16/src/pages/Signup/index.jsx
+++ React JS/class 16/src/pages/Signup/index.jsx
@@ -12,7 +12,7 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";   
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword } from "firebase/auth";   
 import { auth, db } from "../../firebase";    
 import { doc, setDoc } from "firebase/firestore";   
 import { useNavigate } from "react-router-dom";   
