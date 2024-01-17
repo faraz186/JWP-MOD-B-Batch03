7928273ff78e2361f1dcbf6fe1c2a7433a504d9f
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-01-17 11:48:36 +0500

    Update index.jsx

--- React JS/class 16/src/pages/Signup/index.jsx
+++ React JS/class 16/src/pages/Signup/index.jsx
@@ -1,4 +1,4 @@
-import react, { useState } from "react";  
+import react, { useState } from "react";      
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";   
