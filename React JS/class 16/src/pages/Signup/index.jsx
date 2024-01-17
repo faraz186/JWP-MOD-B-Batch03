5e4ea72c3581f24062bebfd25740135951aa2006
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-01-17 11:46:32 +0500

    Update index.jsx

--- React JS/class 16/src/pages/Signup/index.jsx
+++ React JS/class 16/src/pages/Signup/index.jsx
@@ -135,7 +135,7 @@ export default function SignUp() {
             noValidate
             onSubmit={handleSubmit}
             sx={{ mt: 3 }}
-          >
+          >   
             <Grid container spacing={2}>
               <Grid item xs={12} sm={6}>
                 <TextField
