ebeea218ae5018c8c58fcb4a4361a7ead75c9034
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-01-17 11:46:48 +0500

    Update index.jsx

--- React JS/class 16/src/pages/Signup/index.jsx
+++ React JS/class 16/src/pages/Signup/index.jsx
@@ -13,7 +13,7 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth, db } from "../../firebase";
+import { auth, db } from "../../firebase";    
 import { doc, setDoc } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";   
 import { toast } from "react-toastify";
