a2464df72797b740098643256a774487bcc0b677
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-01-27 14:57:09 +0500

    Update App.jsx

--- React JS/class 10/src/App.jsx
+++ React JS/class 10/src/App.jsx
@@ -1,6 +1,6 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'                   
 import './App.css';      
-import Table from './components/Table'         
+import Table from './components/Table'            
 import Button from './components/Button'            
 import Input from './components/Input';
 import Card from './components/Card';     
