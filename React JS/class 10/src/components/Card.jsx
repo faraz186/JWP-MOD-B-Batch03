99c49137f2110c6b573cf48418d4130b0045271e
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-01-31 00:24:41 +0500

    Update Card.jsx

--- React JS/class 10/src/components/Card.jsx
+++ React JS/class 10/src/components/Card.jsx
@@ -3,7 +3,7 @@ import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 function Card(props){
     const {img,id,userName,age,inst,isActive,category} = props
     return(
-        <div className='w-25 border rounded text-center my-5'>
+        <div className='w-25 border rounded text-center my-5'>  
           <p>{img }</p>   
           <p>Name: {userName}</p>
           <p>Institute: {inst}</p>  
