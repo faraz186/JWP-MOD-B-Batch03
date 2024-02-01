00ddd076ae06ca258559997b2826f948c23526ed
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-02-01 19:35:32 +0500

    Update Table.jsx

--- React JS/class 10/src/components/Table.jsx
+++ React JS/class 10/src/components/Table.jsx
@@ -1,6 +1,6 @@
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'   
 
-function Table(props) {
+function Table(props) {  
     const { name, fName, age, rollNo, class1, inst } = props;
     return (  
             <table className='table mt-4 text-center'>
