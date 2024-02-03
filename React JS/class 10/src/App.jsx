94070b099afa8b8abd30e628a00a0c5f6063197d
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-02-04 00:02:10 +0500

    Update App.jsx

--- React JS/class 10/src/App.jsx
+++ React JS/class 10/src/App.jsx
@@ -5,7 +5,7 @@ import Button from './components/Button'
 import Input from './components/Input';
 import Card from './components/Card';       
 import user1 from './images/user1.jpeg'              
-import user2 from './images/user2.jpeg'     
+import user2 from './images/user2.jpeg'       
 import user3 from './images/user3.jpeg'             
 import user4 from './images/user 4.jpeg'        
 import user5 from './images/user 5.jpeg'                 
