e5cac215dc438efe3888a41f6a22f5b323a265c4
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-02-07 14:00:59 +0500

    Update app.js

--- Advance JS/class 07/app.js
+++ Advance JS/class 07/app.js
@@ -4,7 +4,7 @@ let WeatherImage = `https://openweathermap.org/img/wn/10d@2x.png`
 
 let getWeather = async (city)=>           
 {        
-    weather.innerHTML = `<div class="spinner-grow text-success" role="status">         
+    weather.innerHTML = `<div class="spinner-grow text-success" role="status">           
     <span class="visually-hidden">Loading...</span>      
   </div>`  
              
