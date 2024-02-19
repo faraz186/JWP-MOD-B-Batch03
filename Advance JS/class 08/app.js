9ffbacccf69ee2ab3dc3263898e3f99942c43643
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-02-20 02:17:23 +0500

    Update app.js

--- Advance JS/class 08/app.js
+++ Advance JS/class 08/app.js
@@ -5,7 +5,7 @@ function Student1(name,age,city)
     this.city = city
 }
 
-const std2 = new Student1("faraz",22,"karachi")
+const std2 = new Student1("faraz",22,"karachi")  
 console.log(std2)    
 
 class Student{
