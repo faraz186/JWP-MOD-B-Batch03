70ef62f8c391e8ba776bf84ce3e19a3620364568
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-02-28 19:13:29 +0500

    Update app.js

--- Advance JS/class 13/app.js
+++ Advance JS/class 13/app.js
@@ -1,6 +1,6 @@
 var cities = ["karachi", "lahore", "Multan", "islamabad", "karachi"]     
 
-const findValue = cities.find((value, index) => {
+const findValue = cities.find((value, index) => {  
     if (value.toLowerCase() === "karachi") {  
         return true    
     }         
