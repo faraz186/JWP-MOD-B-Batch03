f7b00ffd043f93a6a0d08466288f871111ae0186
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-03-04 19:44:40 +0500

    Update Input.jsx

--- React JS/class 10/src/components/Input.jsx
+++ React JS/class 10/src/components/Input.jsx
@@ -5,7 +5,7 @@ function Input (props){
      <div className=' mt-5 my-3'>
         <input  type={props.typeInput} value={props.valueInput} className='w-25  p-2 border rounded bg-light '  onChange={props.onChangeInput}  />
      </div>
-     ) 
+     )    
 }
 
 export default Input
