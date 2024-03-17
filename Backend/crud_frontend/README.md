5d36a40458d64e063d6124e13905274342b534e5
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-03-18 02:48:38 +0500

    Delete Backend/crud_frontend directory

--- Backend/crud_frontend/README.md
+++
@@ -1,8 +0,0 @@
-git repo
-video link
-
-
-
-https://github.com/JawanTechPk/crud_backend
-
-https://we.tl/t-YhmgN9XqRy
--- Backend/crud_frontend/frontend/src/constant/index.js
+++
@@ -1,2 +0,0 @@
-// export const BASE_URL = "https://impossible-red-sea-lion.cyclic.app/api";
-export const BASE_URL = "http://localhost:5000/api";
