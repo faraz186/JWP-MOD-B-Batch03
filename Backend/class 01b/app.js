b53252cd5e1ec0d9f765579652f782b920a1215b
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-04-24 00:08:06 +0500

    Update app.js

--- Backend/class 01b/app.js
+++ Backend/class 01b/app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");  
 const generateUniqueId = require("generate-unique-id");
-const app = express();
+const app = express();  
 const PORT = process.env.PORT || 8080;   
 
 // users CRUD
