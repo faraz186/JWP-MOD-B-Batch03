299fd229ab408454e910932a729856d6008d1954
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-05-20 01:20:48 +0500

    Update App.jsx

--- React Native/class 01/App.jsx
+++ React Native/class 01/App.jsx
@@ -8,12 +8,12 @@ const App = () => {
   //   console.log('clicked...!')
   // }
   return <> 
-    {/* <Image resizeMode='cover'  source={require('./assets/Screenshot-01.png')}/> */}
-    {/* <Image resizeMode='cover' width={200} height={200} source={{uri:'https://t4.ftcdn.net/jpg/05/47/97/81/360_F_547978128_vqEEUYBr1vcAwfRAqReZXTYtyawpgLcC.jpg'}}/> */}
-    {/* <Text>Hello World! </Text> */}
-    {/* <TouchableOpacity onPress={press_handle}>
+    <Image resizeMode='cover'  source={require('./assets/Screenshot-01.png')}/> */}
+    <Image resizeMode='cover' width={200} height={200} source={{uri:'https://t4.ftcdn.net/jpg/05/47/97/81/360_F_547978128_vqEEUYBr1vcAwfRAqReZXTYtyawpgLcC.jpg'}}/>
+    <Text>Hello World! </Text>
+    <TouchableOpacity onPress={press_handle}>
       <Text>Click here</Text>
-    </TouchableOpacity> */}
+    </TouchableOpacity>
 
 
 
