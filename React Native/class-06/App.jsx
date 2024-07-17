8ea1eab8ce63893951ba26f2697de734c2e5d350
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-07-18 02:15:33 +0500

    Update App.jsx

--- React Native/class-06/App.jsx
+++ React Native/class-06/App.jsx
@@ -6,7 +6,7 @@ import { Geo_Location_Component } from './components/Geo_Location_Component'
 import { Nativebase_Components } from './components/Nativebase_Components'
 import { Firebase_Authentication_Component } from './components/Firebase_Authentication_Component'
 const App = () => {
-  return (
+  return (   
     // <NativeBaseProvider>
     <View style={{flex:1}}>
       {/* <Text style={{textAlign:'center', fontWeight:'bold', fontSize:20 }}>REACT NATIVE APP</Text> */}
