e80fa7c5ec20f51a6bc558c200dd96f760c44d48
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-07-18 02:15:38 +0500

    Update App.jsx

--- React Native/class-06/App.jsx
+++ React Native/class-06/App.jsx
@@ -2,7 +2,7 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Async_Storage_Component } from './components/Async_Storage_Component'
 import { Image_Picker_Component } from './components/Image_Picker_Component'
-import { Geo_Location_Component } from './components/Geo_Location_Component'
+import { Geo_Location_Component } from './components/Geo_Location_Component'   
 import { Nativebase_Components } from './components/Nativebase_Components'
 import { Firebase_Authentication_Component } from './components/Firebase_Authentication_Component'
 const App = () => {
