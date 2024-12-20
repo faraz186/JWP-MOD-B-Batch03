d15e109fa6e7bc74a393de90caabd98a8898a7c0
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-12-20 14:34:47 +0500

    Update app.js

--- Advance JS/class 04/app.js
+++ Advance JS/class 04/app.js
@@ -15,7 +15,7 @@ console.log(Object.entries(obj))
 
 let arr = ["faraz",12,true,{city:"karachi",age:20},false,100,"abc"];  
 
-console.log(arr[3].age)
+console.log(arr[3].age)   
 
 arr[7] = "jawan pakistan"
 console.log(arr)
